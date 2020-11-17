bcb9cf3867e4eda4ee2ea30881c6acaef6f586e1
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-17 14:38:15 +0200

    Updated

--- TextAdventure/PlayerChar.cs
+++ TextAdventure/PlayerChar.cs
@@ -6,29 +6,19 @@ namespace TextAdventure
 {
     class PlayerChar
     {
-        public PlayerChar(string name, string clas, double strenght, double agility, double intelect, double magika, Dictionary<string, List<string>> abilities)
+        public PlayerChar(string name, string klass, Dictionary<string, double> atributes)
         {
             Name = name;
-            Class = clas;
-            Health = 10 * (strenght / 10);
-            Strenght = strenght;
-            Dodge = 1 + (agility / 10);
-            Agility = agility;
-            Intelect = intelect;
-            Magika = magika * (intelect / 10);
-            Abilities = abilities;
-            Inventory = new List<string>();
+            Klass = klass;
+            Atributes = atributes;
+            Abilities = new Dictionary<string, List<string>>();
             Skills = new Dictionary<string, int>();
+            Inventory = new List<string>();
         }
 
         public string Name { get; set; }
-        public string Class { get; set; }
-        public double Health { get; set; }
-        public double Strenght { get; set; }
-        public double Dodge { get; set; }
-        public double Agility { get; set; }
-        public double Intelect { get; set; }
-        public double Magika { get; set; }
+        public string Klass { get; set; }
+        public Dictionary<string, double> Atributes { get; set; }
         public Dictionary<string, List<string>> Abilities { get; set; }
         public Dictionary<string, int> Skills { get; set; }
         public List<string> Inventory { get; set; }
--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TextAdventure
@@ -17,11 +18,16 @@ namespace TextAdventure
             Console.WriteLine("\\EEEEEEEEE//   \\LLLLLLLLL\\   \\DDDDDDDDD//   \\EEEEEEEEEE// ||RR|    \\R\\  \\EEEEEEEEE//");
             Console.WriteLine(" ^^^^^^^^^^    ^^^^^^^^^^^    ^^^^^^^^^^     ^^^^^^^^^^   ^^^^^     ^^^  ^^^^^^^^^^");
             Console.WriteLine("                                 Eldere's Labyrinth");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-            CharacterCreator();
+            
+
+            //This will be the player character for later on
+            PlayerChar player = CharacterCreator();
 
         }
-        static void CharacterCreator()
+        static PlayerChar CharacterCreator()
         {
             Console.Clear();
             //Place Holders
@@ -35,9 +41,48 @@ namespace TextAdventure
             string name = Console.ReadLine();
             string klass = String.Empty;
             Console.WriteLine();
+            Console.Clear();
             Console.WriteLine("What did you do before coming here");
-            Console.WriteLine("A) Were you a mercenery for hire.");
-            Console.WriteLine("B) Or perhaps you studied magic in a local collegev.");
+            //Just put them somewhere here. Make sure to check the way they look first tho
+            //warrior klass 
+            Console.WriteLine();
+            Console.WriteLine("A) Were you a mercenery for hire.Burning towns protecting people.");
+            Console.WriteLine();
+            //Draw them after the introduction not before 
+            Console.WriteLine("     ^^^^^^^^^^^ ");
+            Console.WriteLine(" <//===========\\\\>");
+            Console.WriteLine("<//   _      _  \\\\>");
+            Console.WriteLine("<||  / \\ .. / \\  ||");
+            Console.WriteLine("<||  )  (  )  (  ||>");
+            Console.WriteLine("<||  \\ / || \\ /  ||> ");
+            Console.WriteLine("<||   ^  ||  ^   ||>");
+            Console.WriteLine("<||      ||      ||>");
+            Console.WriteLine("<||      ||      ||>");
+            Console.WriteLine("<||      ||      ||>");
+            Console.WriteLine("<||      ()      ||>");
+            Console.WriteLine("<\\\\             //>");
+            Console.WriteLine(" <\\\\===========//>");
+            Console.WriteLine("    vvvvvvvvvvv");
+            Console.WriteLine();
+            //sourcerer klass 
+            //cw tab tab e shortcut ako taka ti e bavno
+            Console.WriteLine("B) Or perhaps you studied magic in a local college.");
+            Console.WriteLine("");
+            Console.WriteLine(" *//===========\\\\*");
+            Console.WriteLine("*//      /\\     \\\\*");
+            Console.WriteLine("*||   /\\//\\\\/\\   ||* ");
+            Console.WriteLine("*||  /// /\\ \\\\\\  ||* ");
+            Console.WriteLine("*|| ||| (\\/) ||| ||*");
+            Console.WriteLine("*|| ||| |**| ||| ||*");
+            Console.WriteLine("*|| ||| \\\\// ||| ||*");
+            Console.WriteLine("*|| |||  ||  ||| ||*");
+            Console.WriteLine("*||  \\\\\\_||_///  ||*");
+            Console.WriteLine("*||      ||      ||*");
+            Console.WriteLine("*\\\\      ||     //*");
+            Console.WriteLine(" *\\===========//*");
+            Console.WriteLine("    ***********");
+            Console.WriteLine();
+
             while (true)
             {
                 string classSel = Console.ReadLine().ToLower();
@@ -56,33 +101,112 @@ namespace TextAdventure
                     Console.WriteLine("Invalid Class");
                 }
             }
-            //mr taco was here
-            Console.WriteLine($"Very well. So you were a {klass} not suprising considering the way you look.");
-            Console.WriteLine();
-            Console.WriteLine("What were you good at exactly.");
+            //Default attributes for playerChar some might be augmented because of the way the calcs work
             double health = 10;
             double str = 10;
             double agi = 10;
             double intellect = 10;
             double magika = 10;
+
+            //Dictionary for the atributes for easier use and augmentation of stats
+            Dictionary<string, double> atributes = new Dictionary<string, double>();
+            atributes.Add("health", health + Math.Round(str / 10));
+            atributes.Add("strenght", str);
+            atributes.Add("agility", agi);
+            atributes.Add("intellect", intellect);
+            atributes.Add("magika", magika + Math.Round(intellect / 10));
+            atributes.Add("dogde", 10 + Math.Round(agi / 10));
+            atributes.Add("armor", 10 + atributes["dogde"]);
+
             //Taking all atributes for the creator leaving the person to add some more later on
             int pointsToAdd = 10;
-            while (true)
+            PlayerChar player = new PlayerChar(name, klass, atributes);
+
+            //The character creator UI element and wizard
+            //Assigning atributes
+            while (pointsToAdd != 0)
             {
+                //Help for the creator wizard
+                Console.Clear();
+                Console.WriteLine($"Very well. So you were a {klass} not suprising considering the way you look.");
                 Console.WriteLine();
-                Console.WriteLine($"Current stats:\nHealth -> {health + Math.Round(str / 10)}\nStrenght -> {str}\nAgility -> {agi}\nDogde -> {1 + Math.Round(agi / 10)}\nIntellect -> {intellect}\nMagika -> {magika + Math.Round(intellect / 10)}");
+                Console.WriteLine("What were you good at exactly.");
+                Console.WriteLine();
+                Console.WriteLine($"Current stats:\nHealth -> {atributes["health"] + Math.Round(atributes["strenght"] / 10)}" +
+                    $"\nStrenght -> {atributes["strenght"]}" +
+                    $"\nAgility -> {atributes["agility"]}" +
+                    $"\nDogde -> {1 + Math.Round(atributes["agility"] / 10)}" +
+                    $"\nIntellect -> {atributes["intellect"]}" +
+                    $"\nMagika -> {atributes["magika"] + Math.Round(atributes["intellect"] / 10)}");
+                Console.WriteLine();
                 Console.WriteLine($"Left augment points: {pointsToAdd}");
-                Console.WriteLine($"How many do you want to add to an atribute");
-                Console.WriteLine("Atribute: Points:");
+                Console.WriteLine();
+                Console.WriteLine($"How many do you want to add or remove from an atribute");
+                Console.WriteLine("Atribute: Points: Add\\Remove: ");
+
+                //Saving the input in two vars for easier use later on
                 string[] input2 = Console.ReadLine()
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
                 string atribute = input2[0];
                 int pointsToAddToAtrb = int.Parse(input2[1]);
+                string operation = input2[2];
 
+                //Actual code for the wizard
+                if (operation.ToLower() == "add")
+                {
+                    if (atributes.ContainsKey(atribute.ToLower()))
+                    {
+                        if (atributes[atribute] + pointsToAddToAtrb > 20)
+                        {
+                            Console.WriteLine("Atributes cannot be higher than 20.");
+                        }
+                        else
+                        {
+                            atributes[atribute] += pointsToAddToAtrb;
+                            pointsToAdd -= pointsToAddToAtrb;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid atribute");
+                    }
+                }
+                else if (operation.ToLower() == "remove")
+                {
+                    if (atributes.ContainsKey(atribute.ToLower()))
+                    {
+                        if (atributes[atribute] - pointsToAddToAtrb <= 0)
+                        {
+                            Console.WriteLine("Atributes cannot be less than 1.");
+                        }
+                        else
+                        {
+                            atributes[atribute] -= pointsToAddToAtrb;
+                            pointsToAdd += pointsToAddToAtrb;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid atribute");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Operator");
+                }
+                if (pointsToAdd < 0)
+                {
+                    Console.WriteLine("You need to have spend too many atribute points.");
+                }
             }
+            //Adding skills and abilities based on the class and atributes
+
             Console.WriteLine();
             Console.WriteLine("So then would you mind telling me what skills you posses.");
+
+            //returns the player char for easier use in the Main method
+            return player;
         }
     }
 }
